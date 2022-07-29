87a78678027158a5c793cc86ab0c273d4e24b4fc
Istvan Toth <wbtist@gmail.com>
2022-07-29 14:43:43 +0100

    Deice Images
    Creating Dice images

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 // Generate variables -- scores, dice, player_1_Turn, player_2_Turn,
 let scores = [0, 0];
-let player01Score = null
+let player01Score = null;
+let player_1_Turn = document.querySelector('.player--0-turn');
+let player_2_Turn = document.querySelector('.player--1-turn');
+
 // Generate random number 1-6
+let dice = Math.floor(Math.random() * (6 - 1) + 1);
+
