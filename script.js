2557a78e2e8cee58ef76c23d12bfa53218d5bf1b
Istvan Toth <wbtist@gmail.com>
2022-07-31 18:32:16 +0100

    Game logic complete
    The game runs at it needs.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const player_0_htmlScore = document.querySelector('#player--0-score');
 const player_1_htmlScore = document.querySelector('#player--1-score');
 const player_0_htmlturn = document.querySelector('#player--0-turn');
 const player_1_htmlturn = document.querySelector('#player--1-turn');
+const win_Message = document.querySelector('.win-message');
 let scores = [0, 0];
 let currentScore;
 // Current player
@@ -22,6 +23,7 @@ function setDefaults() {
   scores[0] = 0;
   scores[1] = 0;
   dice_image.classList.add('hidden');
+  win_Message.classList.add('hidden');
   player_0_htmlScore.textContent = 0;
   player_0_htmlturn.textContent = 0;
   player_1_htmlScore.textContent = 0;
@@ -60,9 +62,11 @@ hold_Button.addEventListener('click', () => {
   document.querySelector(`#player--${currentPlayer}-score`).textContent = scores[currentPlayer];
   // Clear turn value
   document.querySelector(`#player--${currentPlayer}-turn`).textContent = 0;
-  if (scores[currentPlayer] >= 10) {
-    alert(`Hurray Player-${currentPlayer} win!`);
-    setDefaults();
+  if (scores[currentPlayer] >= 100) {
+    dice_image.classList.add('hidden');
+    win_Message.classList.remove('hidden');
+    // alert(`Hurray Player-${currentPlayer} win!`);
+
   } else {
     switchPlayers();
     currentScore = 0;
@@ -74,71 +78,3 @@ hold_Button.addEventListener('click', () => {
 function switchPlayers() {
   currentPlayer = currentPlayer === 0 ? 1 : 0;
 };
-
-
-/* Declaring const variables*/
-/* Declaring let variables*/
-/* Declaring Functions*/
-// Creating startConditions function
-
-// Creating newGame function
-
-/* Button click event listeners*/
-// New Game Button Click Event
-// Dice Button Click Event
-// Hold button Click Event
-
-
-
-
-
-
-
-
-
-
-
-// Code V0.5
-// let scores = [0, 0];
-// let activePlayer = 0;
-// let player_1_Turn = document.querySelector('.player--0-turn');
-// let player_2_Turn = document.querySelector('.player--1-turn');
-// let dice;
-// let dice_image;
-
-// // Generate random number 1-6
-// function setDefaults() {
-//   scores = [0, 0];
-//   activePlayer = 0;
-// };
-// setDefaults();
-// // Set the dice image based on dice.
-
-
-// document.querySelector('#dice-button').addEventListener('click', diceClicked);
-
-// function diceClicked() {
-//   // On click, generate a random number, display it and if it is not 1, add to plaer`s Turn
-//   dice = Math.floor(Math.random() * (6 - 1) + 1);
-//   dice_image = document.querySelector('.dice-image').setAttribute('src', `img/dice--${dice}.png`);
-//   if (dice !== 1) {
-//     player_1_Turn.textContent = Number(player_1_Turn.textContent) + Number(dice);
-//     console.log('after ', + player_1_Turn.textContent);
-//   }
-
-
-// };
-
-// document.querySelector('#hold-button').addEventListener('click', holdClicked);
-
-// function holdClicked() {
-//   // add turn value to player1s score. If score >=100, player wins
-//   scores[activePlayer] += Number(player_1_Turn.textContent);
-//   alert(scores[activePlayer]);
-// };
-
-// document.querySelector('#newGame-button').addEventListener('click', newGameButtonClicked);
-
-// function newGameButtonClicked() {
-//   alert('New Game button clicked');
-// };
